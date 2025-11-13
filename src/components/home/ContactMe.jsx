1c2ad186969c9a3fb7c11d2e11cc20f76254046e
Marco Fernandez <marco@maverickgraphics.com>
2025-11-13 15:36:01 -0500

    Updated form code and added modal

--- src/components/home/ContactMe.jsx
+++ src/components/home/ContactMe.jsx
@@ -5,12 +5,19 @@ import { contactInfo } from '@/staticData/home/home';
 import { BsArrowRight } from 'react-icons/bs';
 import ContactInfoItem from './ContactInfoItem';
 
+import Modal from "../modal/Modal";
+
 const ContactMe = () => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const openModal = () => setIsModalOpen(true);
+	const closeModal = () => setIsModalOpen(false);
+
 	const [formValues, setFormValues] = useState({
 		firstname: '',
 		laststname: '',
 		email: '',
-		subject: '',
+		subject: 'Message from mayhemarts.com',
 		message: '',
 	});
 
@@ -21,7 +28,7 @@ const ContactMe = () => {
 			firstname: '',
 			lastname: '',
 			email: '',
-			subject: '',
+			subject: 'Message from mayhemarts.com',
 			message: '',
 		});
 	};
@@ -30,11 +37,29 @@ const ContactMe = () => {
 		setFormValues({ ...formValues, [e.target.name]: e.target.value });
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log('formValues', formValues);
+	// const handleSubmit = (e) => {
+	// 	e.preventDefault();
+	// 	console.log('formValues', formValues);
 
-		// reset after form submit
+	// 	// reset after form submit
+	// 	reset();
+	// };
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+
+		const myForm = event.target;
+		const formData = new FormData(myForm);
+
+		fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString()
+		})
+			.then(() => {
+				openModal();
+			})
+			.catch(error => alert(error));
 		reset();
 	};
 
@@ -45,119 +70,124 @@ const ContactMe = () => {
 		// Trigger the hidden submit button
 		submitButtonRef.current.click();
 	};
- 
-	return (
-		<div className="contact" data-title="Contact" id="contact">
-			<div className="container">
-				<div className="section-title">
-					<h2 className="title">
-						<span>Contact</span> Me
-					</h2>
-				</div>
 
-				<div className="row align-items-lg-center">
-					<div className="col-lg-6">
-						<div className="contact-form">
-							<form data-netlify="true" name="contact" onSubmit={handleSubmit}>
-								<div className="row">
-									<div className="col-lg-6">
-										<div className="form-group form-floating">
-											<Input
-												placeholder="First Name"
-												label="First Name"
-												handleChange={handleChange}
-												name="firstname"
-												value={firstname}
-											/>
-										</div>
-									</div>
-									<div className="col-lg-6">
-										<div className="form-group form-floating">
-											<Input
-												placeholder="Last Name"
-												label="Last Name"
-												handleChange={handleChange}
-												name="lastname"
-												value={lastname}
-											/>
-										</div>
-									</div>
-								</div>
-								<div className="row">
-									<div className="col-lg-12">
-										<div className="form-group form-floating">
-											<Input
-												type="email"
-												placeholder="Email Address"
-												label="Email Address"
-												handleChange={handleChange}
-												name="email"
-												value={email}
-											/>
-										</div>
-									</div>
-								</div>
-								<div className="row">
-									<div className="col-lg-12">
-										<div className="form-group form-floating">
-											<Input
-												placeholder="Your Subject"
-												label="Your Subject"
-												handleChange={handleChange}
-												name="subject"
-												value={subject}
-											/>
-										</div>
-									</div>
-								</div>
-								<div className="row">
-									<div className="col-lg-12">
-										<div className="form-group form-floating">
-											<Textarea
-												label="Your Message"
-												handleChange={handleChange}
-												name="message"
-												value={message}
-											/>
-										</div>
-									</div>
-								</div>
-								<div className="row">
-									<div className="col-lg-12">
-										<div className="form-group form-floating">
-											{/* Hidden submit button */}
-											<button
-												type="submit"
-												className="theme-btn border-0"
-												style={{ display: 'none' }}
-												ref={submitButtonRef}
-											>
-												SUBMIT NOW
-											</button>
-											{/* Custom submit button */}
-											<button
-												type="button"
-												className="theme-btn border-0"
-												onClick={handleCustomSubmit}
-											>
-												SUBMIT NOW <BsArrowRight className="ms-2" size={25} />
-											</button>
-										</div>
-									</div>
-								</div>
-							</form>
-						</div>
+	// document.querySelector("contact").addEventListener("submit", handleSubmit);
+
+	return (
+		<>
+			<div className="contact" data-title="Contact" id="contact">
+				<div className="container">
+					<div className="section-title">
+						<h2 className="title">
+							<span>Contact</span> Me
+						</h2>
 					</div>
-					<div className="col-lg-6">
-						<div className="contact-info">
-							{contactInfo?.map((item) => (
-								<ContactInfoItem key={item?.id} {...item} />
-							))}
+
+					<div className="row align-items-lg-center">
+						<div className="col-lg-6">
+							<div className="contact-form">
+								<form data-netlify="true" data-netlify-recaptcha="true" id="contact" name="contact" onSubmit={handleSubmit}>
+									<div className="row">
+										<div className="col-lg-6">
+											<div className="form-group form-floating">
+												<Input
+													placeholder="First Name"
+													label="First Name"
+													handleChange={handleChange}
+													name="firstname"
+													required="required"
+													value={firstname}
+												/>
+											</div>
+										</div>
+										<div className="col-lg-6">
+											<div className="form-group form-floating">
+												<Input
+													placeholder="Last Name"
+													label="Last Name"
+													handleChange={handleChange}
+													name="lastname"
+													required="required"
+													value={lastname}
+												/>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-lg-12">
+											<div className="form-group form-floating">
+												<Input
+													type="email"
+													placeholder="Email Address"
+													label="Email Address"
+													handleChange={handleChange}
+													name="email"
+													required="required"
+													value={email}
+												/>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-lg-12">
+											<div className="form-group form-floating">
+												<Textarea
+													label="Your Message"
+													handleChange={handleChange}
+													name="message"
+													value={message}
+												/>
+											</div>
+										</div>
+									</div>
+									<div className="row">
+										<div className="col-lg-12">
+											<div data-netlify-recaptcha="true"></div>
+											<div className="form-group form-floating">
+												{/* Hidden submit button */}
+												<button
+													type="submit"
+													className="theme-btn border-0"
+													style={{ display: 'none' }}
+													ref={submitButtonRef}
+												>
+													SUBMIT NOW
+												</button>
+												{/* Custom submit button */}
+												<button
+													type="button"
+													className="theme-btn border-0"
+													onClick={handleCustomSubmit}
+												>
+													SUBMIT NOW <BsArrowRight className="ms-2" size={25} />
+												</button>
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+						<div className="col-lg-6">
+							<div className="contact-info">
+								{contactInfo?.map((item) => (
+									<ContactInfoItem key={item?.id} {...item} />
+								))}
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<Modal isOpen={isModalOpen} onClose={closeModal}>
+				<h2>Thank you!</h2>
+				<p className='mt-4'>
+					I've received your message. Kindly allow 24-48 hours for responses. <br />
+					Please call or email if you require a more immediate response.
+				</p>
+
+				<button className="mt-5 theme-btn border-0" onClick={closeModal}>Okay</button>
+			</Modal>
+		</>
 	);
 };
 
